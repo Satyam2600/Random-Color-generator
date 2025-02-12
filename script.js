5ea6beffa579699fbfbe38ab5c92219138f18f8e
satyam <satyam.sk.026@gmail.com>
2025-02-12 12:09:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("ni1bgib");
+function random_number_generator() {
+    let r = Math.ceil(0 + Math.random() * 256)
+    let g = Math.ceil(0 + Math.random() * 256);
+    let b = Math.ceil(0 + Math.random() * 256);
+    return `rgb ${r}, ${g}, ${b}`;
+
+}
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach(box => {
+bo
+
+})
+
+        
+   

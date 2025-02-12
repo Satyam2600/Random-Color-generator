e6114b286bb457abb7b9e000ef5a3896d72df4cc
satyam <satyam.sk.026@gmail.com>
2025-02-12 12:26:52 +0530

    did some modifications coz i fking fcked the spellings

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log("ni1bgib");
 function random_number_generator() {
-    let r = Math.ceil(0 + Math.random() * 256)
-    let g = Math.ceil(0 + Math.random() * 256);
-    let b = Math.ceil(0 + Math.random() * 256);
-    return `rgb ${r}, ${g}, ${b}`;
+    let r = Math.ceil(0 + Math.random() * 255)
+    let g = Math.ceil(0 + Math.random() * 255);
+    let b = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
 
 }
 let boxes = document.getElementsByClassName("box");
 
 Array.from(boxes).forEach(box => {
-    box.style.backgroundcolor = random_nuber_generator();
+    box.style.backgroundColor = random_number_generator();
     box.style.color = random_number_generator();
 
 })
